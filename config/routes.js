20e76f16621274f5251dc181b38766dcd27abaeb
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-06-15 18:39:16 +0700

    update file

--- config/routes.js
+++ config/routes.js
@@ -34,6 +34,18 @@ apiRouter.post("/api/v1/auth/google",);
 //authenticate user
 apiRouter.get("/api/v1/whoami",);
 
+//logout
+apiRouter.get("/api/v1/logout",);
+
+//add item
+apiRouter.post("/api/v1/items/add",);
+
+//update item description
+apiRouter.post("/api/v1/items/update/:id",);
+
+//update user profile
+apiRouter.post("/api/v1/profile/update/:id",);
+
 //route not found
 apiRouter.use();
 apiRouter.use();
