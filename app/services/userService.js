85fbbfe15e03cce819271949334fdb91046e4b38
jryarianto <jerry.arianto@student.umn.ac.id>
2022-06-15 20:13:02 +0700

    userRepository.js and userService.js

---
+++ app/services/userService.js
@@ -0,0 +1,31 @@
+const userRepository = require("../repositories/userRepository");
+
+module.exports = {
+  async create(requestBody) {
+    return userRepository.create(requestBody);
+  },
+
+  //   async update(id, requestBody) {
+  //     return userRepository.update(id, requestBody);
+  //   },
+
+  //   async delete(id) {
+  //     return userRepository.delete(id);
+  //   },
+
+  //   async find(id) {
+  //     return userRepository.find(id);
+  //   },
+
+  //   async list() {
+  //     try {
+  //       const type_user = await userRepository.findAll();
+
+  //       return {
+  //         data: type_user,
+  //       };
+  //     } catch (err) {
+  //       throw err;
+  //     }
+  //   },
+};
