06eab706c829b8984bde1b39264829b4d7ed8528
renmln <renamuliani@gmail.com>
2022-06-20 21:36:47 +0700

    update

--- app/controllers/api/v1/userController.js
+++ app/controllers/api/v1/userController.js
@@ -1,16 +1,17 @@
-const userService = require('../../../repositories');
+const userService = require("../../../repositories/userRepository");
 
 module.exports = {
-    async updateProfile(req, res) {
-        const user = await userService.update(req.params.id, req.body)
-        .then((user) => {
-            res.status(201).json(user);
-        })
-        .catch((err) => {
-            res.status(422).json({
-                status: "FAIL",
-                message: err.message,
-            });
-        })
-    }
-}
+  async updateProfile(req, res) {
+    const user = await userService
+      .update(req.params.id, req.body)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch((err) => {
+        res.status(422).json({
+          status: "FAIL",
+          message: err.message,
+        });
+      });
+  },
+};
