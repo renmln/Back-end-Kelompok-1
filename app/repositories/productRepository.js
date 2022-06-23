9d949bed6d090c7f9f31466c8d4d296462e20f86
renmln <renamuliani@gmail.com>
2022-06-23 20:56:52 +0700

    update productRepo

--- app/repositories/productRepository.js
+++ app/repositories/productRepository.js
@@ -1,36 +1,54 @@
 const { Product } = require("../models");
-const { Offering } = require("../models");
+const { Category } = require("../models");
 
 module.exports = {
-    create(createArgs) {
-        return Product.create(createArgs);
-    },
-    update(id, updateArgs){
-        return Product.update(updateArgs, {
-            where: {
-                id
-            }
-        })
-    },
-    findAll() {
-        return Product.findAll({
-            where: {
-                status : null 
-            }
-        })
-    },
-    findOne(id) {
-        return Product.findOne({
-            where: {
-                id_seller : id
-            }
-        })
-    },
-    findByPk(id) {
-        return Offering.findByPk({
-            where: {
-                id
-            }
-        })
-    }
+  create(createArgs) {
+    return Product.create(createArgs);
+  },
+  update(id, updateArgs) {
+    return Product.update(updateArgs, {
+      where: {
+        id,
+      },
+    });
+  },
+  findAll() {
+    return Product.findAll(
+      {
+        include: [
+          {
+            model: Category,
+          },
+        ],
+      },
+      {
+        where: {
+          status: null,
+        },
+      }
+    );
+  },
+  findOne(id) {
+    return Product.findOne({
+      where: {
+        id_seller: id,
+      },
+    });
+  },
+  findByCategory(id) {
+    return Product.findOne(
+      {
+        include: [
+          {
+            model: Category,
+          },
+        ],
+      },
+      {
+        where: {
+          id_category: id,
+        },
+      }
+    );
+  },
 };
