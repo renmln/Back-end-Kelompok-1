49395e3b9f9dbbe489dfc1613f70d4912184fb62
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-07-01 20:32:06 +0700

    update file

--- app/controllers/api/v1/googleController.js
+++ app/controllers/api/v1/googleController.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const userService = require('../../../services/userService')
 const { OAuth2Client } = require("google-auth-library");
