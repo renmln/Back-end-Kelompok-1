06361fa63717b06d380e089279f0a45ed4e0521e
renmln <renamuliani@gmail.com>
2022-07-22 16:15:27 +0700

    update dikit

--- config/database.js
+++ config/database.js
@@ -34,7 +34,7 @@ module.exports = {
     dialect: "postgres",
     dialectOptions: {
       ssl: {
-        require: true,
+        require: false,
         rejectUnauthorized: false,
       },
     },
