a8d0003e262e7b042f0b198d91b06f0a55d72ac9
renmln <renamuliani@gmail.com>
2022-07-22 12:26:07 +0700

    update

---
+++ db/seeds/20220722051632-add-user.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "superadmin@gmail.com",
+          password: await encryptPassword("super123"),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("User", null, {});
+  },
+};
