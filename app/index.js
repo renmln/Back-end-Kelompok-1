913e51357f32593a3c782bd8a14533253d58d124
renmln <renamuliani@gmail.com>
2022-07-23 16:07:03 +0700

    update cors

--- app/index.js
+++ app/index.js
@@ -10,17 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   connectionString:
-//     process.env.DATABASE_URL ||
-//     "postgresql://postgres:a34ac7ec23d2440991afd9d1896dfd047a18e9814b33af2a8a3f0fa9610d7957@localhost:5432/d537ovteft4pgp",
-//   ssl: process.env.DATABASE_URL ? true : false,
-// });
-
-// pool.connect();
-
 app.use(router);
 router.use(express.urlencoded({ extended: true }));
 
