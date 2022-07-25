dcf1ec513a4f97c0feda11fb73c0b0d5cb8f9b4e
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-07-25 19:03:57 +0700

    add documentations (unfinish)

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ const app = express();
 const router = require("../config/routes");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
 
 // Set format request
 app.use(express.json());
@@ -12,5 +14,8 @@ app.use(cookieParser());
 
 app.use(router);
 router.use(express.urlencoded({ extended: true }));
+app.get('/documentation.json', (req, res) => res.send(swaggerDocument));
+app.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 module.exports = app;
